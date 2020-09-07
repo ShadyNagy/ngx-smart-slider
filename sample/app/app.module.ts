4590e4f21b0d9893f6b782b742bd54da8f5c96fd
ShadyNagy <info@shadynagy.com>
2020-09-08 01:14:09 +0200

    sdsd

--- sample/app/app.module.ts
+++ sample/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import {NgxSmartSliderModule} from 'ngx-smart-slider';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
