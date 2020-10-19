6ccccedbf01958fb1bf0da018dec38558f251006
ShadyNagy <info@shadynagy.com>
2020-10-20 01:53:30 +0200

    v1.1.6

--- projects/ngx-smart-slider/src/public-api.ts
+++ projects/ngx-smart-slider/src/public-api.ts
@@ -5,4 +5,5 @@ export * from './lib/ngx-smart-slider.module';
 
 export * from './lib/smart-slider/smart-slider.service';
 export * from './lib/smart-slider/smart-slider.component';
+export * from './lib/smart-slider/smart-slider-vertical-v2/smart-slider-vertical-v2.component';
 export * from './lib/smart-slider/smart-slider.module';
