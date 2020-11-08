e93c7d896f7e2553ebbfd3907f802df21e5ee1ec
ShadyNagy <info@shadynagy.com>
2020-11-08 18:13:26 +0200

    clean

--- dist/ngx-smart-slider/lib/models/smart-slider-item.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class SmartSliderItem {
-    data?: {};
-    id?: string;
-    text?: string;
-    image?: string;
-    alt?: string;
-    isSelected?: boolean;
-}
--- dist/ngx-smart-slider/lib/ngx-smart-slider.module.d.ts
+++
@@ -1,9 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common';
-import * as ɵngcc2 from './smart-slider/smart-slider.module';
-export declare class NgxSmartSliderModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NgxSmartSliderModule, never, [typeof ɵngcc1.CommonModule, typeof ɵngcc2.SmartSliderModule], [typeof ɵngcc2.SmartSliderModule]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<NgxSmartSliderModule>;
-}
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmd4LXNtYXJ0LXNsaWRlci5tb2R1bGUuZC50cyIsInNvdXJjZXMiOlsibmd4LXNtYXJ0LXNsaWRlci5tb2R1bGUuZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQTs7O0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVjbGFyZSBjbGFzcyBOZ3hTbWFydFNsaWRlck1vZHVsZSB7XHJcbn1cclxuIl19
